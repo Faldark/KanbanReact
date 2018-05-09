adec6a975152752227f6d3ee179d5740375f68fc
BChukhno <bogdan.chukhno@gmail.com>
2018-05-09 13:04:37 +0300

    Routing Error

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import {BrowserRouter,Route} from 'react-router-dom'
+
 import App from './App';
+import Dashboard from './Dashboard';
+
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+
+
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+  <div>
+  <Route exact path="/" component={App}/>
+  <Route path="/dashboard" component={Dashboard}/> 
+  
+    
+  </div>
+    
+  </BrowserRouter>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
